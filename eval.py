b6e7a161175595c44e6bb399bb455dd1de48d7ca
JaywongWang <525021930@qq.com>
2020-01-05 00:18:25 +0800

    update

--- eval.py
+++ eval.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--result_file', type=str, default='./results/charades/predict_proposals_test_nms_0.55.json')
-    parser.add_argument('--gt_file', type=str, default='./datasets/charades/data/test.json')
+    parser.add_argument('--gt_file', type=str, default='./datasets/charades/data/save/test.json')
     args = parser.parse_args()
 
     eval_result(args.result_file, args.gt_file)
